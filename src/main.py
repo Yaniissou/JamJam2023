d79acc35ae6c89acb70279abfc1bb1f7297aebac
Fayhild <gabriel.schaal27@gmail.com>
2023-11-07 13:39:19 +0100

    police size changed on landing screen

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ pygame.font.init()
 pygame.mixer.init()
 
 clock = pygame.time.Clock()
-font = pygame.font.Font("fonts/Minecraft.ttf", 36)
+font = pygame.font.Font("fonts/Minecraft.ttf", 72)
 favicon = pygame.image.load("assets/favicon/favicon.png")
 pygame.display.set_icon(favicon)
 
